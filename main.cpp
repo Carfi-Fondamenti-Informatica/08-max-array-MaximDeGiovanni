5cd66b9fe2ad95ba478599b41d6f8c6a0c3aeb4a
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-12-12 16:07:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-int main() {
-    char n=0;
-    cin>>n;
-    if (lettera(n)==true) {
-        cout<< n <<endl;
-    } else {
-        cout<<"errore"<<endl; }
+int main () {
+   int n=0;
+   cin>>n;
+   float ar[n];
+   for (int i=0; i<n; i++){
+       cin>>ar[i];
+   }
+ cout<< max (ar , n)<<endl;
     return 0;
 }
