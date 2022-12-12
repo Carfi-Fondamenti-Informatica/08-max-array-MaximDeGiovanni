9c3e6f9786c0d35990975759aa912962ae31174e
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-12-12 16:00:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
     char n=0;
     cin>>n;
-    if (lettera(n)==true)
+    if (lettera(n)==true) {
         cout<< n <<endl;
-    else
-        cout<<"errore"<<endl;
+    } else {
+        cout<<"errore"<<endl; }
     return 0;
 }
