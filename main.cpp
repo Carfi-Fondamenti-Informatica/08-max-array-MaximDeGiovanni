62040a929c1bb121973ff243360fb92566f4ac7a
MaximDeGiovanni <114994423+MaximDeGiovanni@users.noreply.github.com>
2022-12-12 15:57:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-
 int main() {
-    int dim;
-    cin>>dim;
-    float numeri [dim];
-    for (int i=0; i<dim; i++){
-        cin>>numeri[i];
-    }
-    numeromassimo(numeri, dim);
-    cout<< numeromassimo (numeri, dim)<<endl;
+    char n=0;
+    cin>>n;
+    if (lettera(n)==true)
+        cout<< n <<endl;
+    else
+        cout<<"errore"<<endl;
     return 0;
-    
 }
+
